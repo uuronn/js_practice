fc43ab1870f00c9f412f77bef44a74ed3c642f34
uuronn <uuronprogram@gmail.com>
2021-10-10 02:08:31 +0900

    Add new file

---
+++ main.js
@@ -0,0 +1,15 @@
+document.getElementById("#mainData");
+
+window.addEventListener('load', function() {
+  console.log("test")
+})
+
+window.addEventListener = function(){
+ console.log('チョコレート');
+}
+window.onload = function(){
+ console.log('アイスクリーム');
+}
+window.onload = function(){
+ console.log('プリン');
+}
